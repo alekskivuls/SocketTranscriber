dec23721e511ccd5bf144a731b882150671f6ab2
Aleks Kivuls <akivuls@yahoo.com>
2017-01-14 17:57:18 -0800

    Setup socket transcriber

--- src/main/java/Transcriber.java
+++ src/main/java/Transcriber.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 
 import edu.cmu.sphinx.api.Configuration;
@@ -8,46 +10,64 @@ import edu.cmu.sphinx.api.StreamSpeechRecognizer;
 import edu.cmu.sphinx.decoder.adaptation.Stats;
 import edu.cmu.sphinx.decoder.adaptation.Transform;
 
-public class Transcriber {       
-                                     
-    public static void main(String[] args) throws Exception {
-                                     
-        Configuration configuration = new Configuration();
+public class Transcriber {
+	Configuration configuration;
+	StreamSpeechRecognizer recognizer;
+	InputStream stream;
 
-        configuration
-                .setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
-        configuration
-                .setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
-        configuration
-                .setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+	public Transcriber() throws IOException {
+		configuration = new Configuration();
+		configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+		configuration.setDictionaryPath("resource:/edu/cmu/sphinx/models/en-us/cmudict-en-us.dict");
+		configuration.setLanguageModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us.lm.bin");
+		recognizer = new StreamSpeechRecognizer(configuration);
+	}
 
-        StreamSpeechRecognizer recognizer = new StreamSpeechRecognizer(
-                configuration);
-        
-     // Live adaptation to speaker with speaker profiles
-        InputStream stream = new FileInputStream(new File("/home/akivuls/workspace/SpeechToText/10001-90210-01803.wav"));
-        stream.skip(44);
-        // Stats class is used to collect speaker-specific data
-        Stats stats = recognizer.createStats(1);
-        recognizer.startRecognition(stream);
-        SpeechResult result;
-        while ((result = recognizer.getResult()) != null) {
-            stats.collect(result);
-        }
-        recognizer.stopRecognition();
+	/**
+	 * Transcribes audio from a specified file, writes result to file, then
+	 * returns resulting filepath. Uses adaptive speaker profiles to adjust data
+	 * for better analysis
+	 * 
+	 * @param filepath The audio path to analyze
+	 * @return The filepath to the resulting text transcribed from the audio
+	 * @throws Exception
+	 */
+	public String readFile(String filePath) throws Exception {
+		stream = new FileInputStream(new File(filePath));
+		// Skip first 44 bytes of header of wav files
+		stream.skip(44);
+		
+		// Stats class is used to collect speaker-specific data
+		Stats stats = recognizer.createStats(1);
+		recognizer.startRecognition(stream);
+		SpeechResult result;
+		while ((result = recognizer.getResult()) != null) {
+			stats.collect(result);
+		}
+		recognizer.stopRecognition();
 
-        // Transform represents the speech profile
-        Transform transform = stats.createTransform();
-        recognizer.setTransform(transform);
+		// Transform represents the speech profile
+		Transform transform = stats.createTransform();
+		recognizer.setTransform(transform);
 
-        // Decode again with updated transform
-        stream = new FileInputStream(new File("10001-90210-01803.wav"));
-        stream.skip(44);
-        recognizer.startRecognition(stream);
-        while ((result = recognizer.getResult()) != null) {
-            System.out.format("Hypothesis: %s\n", result.getHypothesis());
-        }
-        recognizer.stopRecognition();
-        
-    }
+		// Decode again with updated transform
+		stream = new FileInputStream(new File(filePath));
+		stream.skip(44);
+		recognizer.startRecognition(stream);
+		StringBuilder sb = new StringBuilder();
+		while ((result = recognizer.getResult()) != null) {
+			String hypothesis = result.getHypothesis();
+			System.out.format("Hypothesis: %s\n", hypothesis);
+			sb.append(hypothesis);
+			sb.append(' ');
+		}
+		recognizer.stopRecognition();
+		
+		File temp = File.createTempFile("transcript" + System.currentTimeMillis(), ".txt");
+		FileWriter out = new FileWriter(temp);
+		out.write(sb.toString());
+		out.close();
+		
+		return temp.getAbsolutePath();
+	}
 }
